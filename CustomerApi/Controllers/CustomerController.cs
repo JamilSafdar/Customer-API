3f7fc065a435513f4de25f6a57fe74fd5062531a
Jamil Safdar <jamil.safdar@ao.com>
2020-11-12 15:50:01 +0000

    Created a customer class with GET,POST etc

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Jamil";
+        }
+
+        //[HttpPost]
+        //public string Post(string title, string firstName, string surname, int age)
+        //{
+        //    return $"Customer created: {title} {firstName} {surname} {age}";
+        //}
+
+        [HttpPost]
+        public string Post(string title, string firstName, string surname, int age)
+        {
+            return $"Customer created: {title} {firstName} {surname} {age}";
+        }
+
+        [HttpPost("{firstName}")]
+        public string PostWithRouteParams(string firstName)
+        {
+            return $"Customer created to: {firstName}";
+        }
+
+        [HttpDelete]
+        
+        public string Delete()
+        {
+            return $"Customer Removed";
+        }
+
+        [HttpPut]
+
+        public string Put()
+        {
+            return $"Customer updated";
+        }
+    }
+}
